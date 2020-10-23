924db54b85976373bd94a9fc95be23c7246be9b8
Olivier Boesch <olivier.boesch@ac-aix-marseille.fr>
2020-10-23 23:44:35 +0200

    deleted unused methods

--- src/main.py
+++ src/main.py
@@ -295,13 +295,6 @@ class PylapseApp(App):
         # TODO: find a way to get ffmpeg stats...
         pass
 
-    @staticmethod
-    def delete_images():
-        """delete_images : empty the images dir"""
-        Logger.info("Timelapse: deleting images in the \"images\" dir")
-        import os
-        os.system('rm images/*')
-
 
 # Run the app -------
 app = PylapseApp()
