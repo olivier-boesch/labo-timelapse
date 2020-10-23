bb1ebd85add81ef2c20da313c2f1193943b16cd9
Olivier Boesch <olivier.boesch@ac-aix-marseille.fr>
2020-10-23 23:43:23 +0200

    Running !

--- src/main.py
+++ src/main.py
@@ -3,24 +3,32 @@
 PyLapse : A python/kivy application to create Timelapses
 Olivier Boesch (c) 2020
 """
+
+# imports -------
+
+# kivy stuff
 from kivy.app import App
-from kivy.uix.popup import Popup
-from kivy.core.window import Window
 from kivy.logger import Logger
 from kivy.clock import Clock
 from kivy.factory import Factory
+
+# time related
 from datetime import timedelta
 from time import sleep
 
+# rpi camera
+import picamera
 
+
+# main app ------
 class PylapseApp(App):
     """PylapseApp : Kivy Application to create Timelapse videos on the Raspberry pi"""
     cam = picamera.PiCamera()  # camera object
+    preview_on = False  # is preview running
     rotation_angle = 0  # rotation angle for the camera (0, 90 180 270°)
     h_flip = False  # horizontal mirroring (boolean)
     v_flip = False  # vertical mirroring (boolean)
-    resolution_preview = '720p'  # default resolution for preview
-    resolution = '1080p'  # default resolution for timelapse (pictures)
+    resolution = '1080p'  # default resolution for timelapse and preview
     timelapse_event = None  # Timelapse event to take picture
     interval_time = 10  # s - interval between two pictures
     total_time = 60  # s - total time of timelapse
@@ -28,30 +36,41 @@ class PylapseApp(App):
     timelapse_started = False  # flag set to True when timelapse is started
     confirm_popup = None  # popup to confirm timelapse stop
 
-    def preview(self, state):
+    def on_start(self):
+        """when app starts"""
+        self.toggle_preview(on=True)
+
+    def set_preview_dims(self, pos, size):
+        """set preview overlay coordinates : pos ansd size"""
+        x, y = pos
+        w, h = size
+        x = int(x)
+        # axle is reversed
+        y = int(self.root_window.size[1] - h - y)
+        w = int(w)
+        h = int(h)
+        self.cam.preview_window = (x,y,w,h)
+
+    def on_stop(self):
+        """when app stops"""
+        self.toggle_preview(on=False)
+
+    def toggle_preview(self, on):
         """preview : Toggle display of the preview"""
         # show preview if the button is down
-        if state == 'down':
-            # make preview overlay fit the preview image widget (it's drawn on top of the app)
-            x, y = self.root.ids['preview'].pos
-            w, h = self.root.ids['preview'].size
-            x = int(x)
-            y = int(Window.size[1] - h - y)
-            w = int(w)
-            h = int(h)
-            # set chosen resolution
-            self.cam.resolution = self.resolution_preview
+        if on:
+            self.preview_on = True
             # display preview
             self.cam.start_preview(fullscreen=False,
-                                   window=(x, y, w, h),
                                    rotation=self.rotation_angle,
                                    hflip=self.h_flip,
                                    vflip=self.v_flip)
+            self.set_preview_dims(self.root.ids['preview'].pos, self.root.ids['preview'].size)
             # a bit of log
             Logger.info("Camera: preview enabled")
-        # button is normal
         else:
             # stop preview
+            self.preview_on = False
             self.cam.stop_preview()
             # bit of log
             Logger.info("Camera: preview disabled")
@@ -73,26 +92,29 @@ class PylapseApp(App):
         # in case of wrong value : set to 0°
         else:
             self.rotation_angle = 0
-        # if the preview is on : update instantly
-        if self.root.ids['toggle_preview'].state == 'down':
-            self.cam.rotation = self.rotation_angle
+        self.cam.rotation = self.rotation_angle
 
-    def preview_resolution_change(self, text):
-        """preview_resolution_change: change resolution of preview only"""
-        if text == '640x480':
-            self.resolution_preview = text
-        elif text == '800x600':
-            self.resolution_preview = text
-        elif text == 'HD 1280x720p':
-            self.resolution_preview = '720p'
-        elif text == 'FHD 1920x1080p':
-            self.resolution_preview = '1080p'
-        # in case of wrong value: set to HD
+    def consistent_image_toggle(self, state_active):
+        """consistent_image_toggle : set or unset 50Hz flickering filter and consistent image settings across time"""
+        if state_active:
+            # Set ISO to the desired value
+            # camera.iso = 100
+            self.cam.exposure_mode = 'verylong'
+            sleep(0.5)
+            # Now fix the values
+            # adjust speed as a multiple of 10ms to prevent 50Hz flickering (multiple of 100Hz -> 10000µs)
+            # with a min of 10ms
+            Logger.info("Camera: Shutter speed is {:3.0f}µts".format(self.cam.exposure_speed))
+            speed = max((self.cam.exposure_speed // 10000) * 10000, 10000)
+            self.cam.shutter_speed = speed
+            Logger.info("Camera: Shutter speed set to {:3.0f}µs".format(speed))
+            g = self.cam.awb_gains
+            self.cam.awb_mode = 'incandescent'
+            self.cam.awb_gains = g
         else:
-            self.resolution_preview = '720p'
-        # if the preview is on : update instantly
-        if self.root.ids['toggle_preview'].state == 'down':
-            self.cam.resolution = self.resolution_preview
+            self.cam.shutter_speed = 0
+            self.cam.exposure_mode = 'auto'
+            self.cam.awb_mode = 'auto'
 
     def resolution_change(self, text):
         """resolution_change : change resolution of the timelapse"""
@@ -110,18 +132,17 @@ class PylapseApp(App):
         else:
             self.resolution = '1080p'
         self.update_ui_time_info()
+        self.cam.resolution = self.resolution
 
     def hflip_change(self, active):
         """hflip_change: set or unset horizontal flip"""
         self.h_flip = active
-        if self.root.ids['toggle_preview'].state == 'down':
-            self.cam.hflip = self.h_flip
+        self.cam.hflip = self.h_flip
 
     def vflip_change(self, active):
         """vflip_change: set or unset vertical flip"""
         self.v_flip = active
-        if self.root.ids['toggle_preview'].state == 'down':
-            self.cam.vflip = self.v_flip
+        self.cam.vflip = self.v_flip
 
     def interval_time_change(self):
         """"interval_time_change: change interval time and reflect on ui"""
@@ -153,21 +174,9 @@ class PylapseApp(App):
         total = self.total_time
         # compute number of frames
         number_of_frames = int(total / every)
-        # compute size of files (in kb or Mb)
-        if self.resolution == '640x480':
-            one_file_size = 10
-        elif self.resolution == '800x600':
-            one_file_size = 20
-        elif self.resolution == '720p':
-            one_file_size = 400
-        elif self.resolution == '1080p':
-            one_file_size = 1000
-        elif self.resolution == '2592x1944':
-            one_file_size = 2400
-        total_size = int(one_file_size * number_of_frames / 1024)  # compute in Mb (approx)
         # update infos label
-        s = "Ready to take {:d} frames : 1 frame every {:d}s; total time {:d}s; frame size {:d}ko; total size {:d}Mb"
-        self.root.ids['infos'].text = s.format(number_of_frames, every, total, one_file_size, total_size)
+        s = "Ready to take {:d} frames : 1 frame every {:d}s; total time {:d}s"
+        self.root.ids['infos'].text = s.format(number_of_frames, every, total)
 
     @staticmethod
     def compute_time(value, unit):
@@ -185,10 +194,7 @@ class PylapseApp(App):
         """timelapse_toggle : start or stop timelapse"""
         # start timelapse if not started
         if not self.timelapse_started:
-            # stop preview to properly display the dialog
-            if self.root.ids['toggle_preview'].state == 'down':
-                self.cam.stop_preview()
-                self.root.ids['toggle_preview'].state = 'normal'
+            self.change_ui_for_timelapse(start_timelapse=True)
             self.confirm_popup = Factory.DialogConfirmDelete()
             self.confirm_popup.open()
         # or stop if it was running
@@ -205,30 +211,13 @@ class PylapseApp(App):
         self.delete_images()
         self.timelapse_event = Clock.schedule_interval(lambda dt: self.perform_capture(), self.interval_time)
         self.frame_counter = 0
-        self.cam.resolution = self.resolution
-        self.cam.rotation = self.rotation_angle
-        self.cam.hflip = self.h_flip
-        self.cam.vflip = self.v_flip
-        # enable annotations on images
+        # enable annotations on images (white text on black background)
         if self.root.ids['add_time'].active:
             self.cam.annotate_background = picamera.Color('black')
             self.cam.annotate_foreground = picamera.Color('white')
         # make image consistent across time and avoid 50Hz flickering
         if self.root.ids['consistent_images'].active:
-            # Set ISO to the desired value
-            camera.iso = 100
-            # Wait for the automatic gain control to settle
-            sleep(2)
-            # Now fix the values
-            # adjust speed as a multiple of 10ms to prevent 50Hz flickering
-            # with a min of 10ms
-            speed = min(camera.exposure_speed // 10000 * 10000, 10000)
-            camera.shutter_speed = speed
-            Logger.info("Camera: Shutter speed set to {:3.0f}ms".format(speed))
-            camera.exposure_mode = 'off'
-            g = camera.awb_gains
-            camera.awb_mode = 'off'
-            camera.awb_gains = g
+            self.consistent_image_toggle(state_active=True)
         self.change_ui_for_timelapse(start_timelapse=True)
         self.perform_capture()
         self.timelapse_started = True
@@ -239,22 +228,21 @@ class PylapseApp(App):
         if self.confirm_popup is not None:
             self.confirm_popup.dismiss()
             self.confirm_popup = None
+        self.cam.annotate_background = None
+        self.cam.annotate_foreground = picamera.Color('white')
+        self.cam.annotate_text = ""
         self.timelapse_event.cancel()
         self.timelapse_event = None
-        Logger.info("Timelapse: Stopped ({:d} frames captured)\nReady".format(self.frame_counter))
+        Logger.info("Timelapse: Stopped ({:d} frames captured)".format(self.frame_counter))
         self.frame_counter = 0
         self.timelapse_started = False
-        self.change_ui_for_timelapse(start_timelapse=True)
+        self.change_ui_for_timelapse(start_timelapse=False)
 
     def change_ui_for_timelapse(self, start_timelapse=True):
         """change_ui_for_timelapse: disable/enable everything possible for/after timelapse"""
         if start_timelapse:
+            self.toggle_preview(on=False)
             self.root.ids['timelapse_toggle'].text = 'Stop Timelapse'
-            if self.root.ids['toggle_preview'].state == 'down':
-                self.cam.stop_preview()
-                self.root.ids['toggle_preview'].state = 'normal'
-            self.root.ids['toggle_preview'].disabled = True
-            self.root.ids['resolution_preview'].disabled = True
             self.root.ids['resolution_timelapse'].disabled = True
             self.root.ids['rotation'].disabled = True
             self.root.ids['hflip'].disabled = True
@@ -266,15 +254,13 @@ class PylapseApp(App):
             self.root.ids['show_images'].disabled = True
             self.root.ids['add_time'].disabled = True
             self.root.ids['consistent_images'].disabled = True
-            self.root.ids['create_video'].disabled = True
-            self.root.ids['delete_images'].disabled = True
+            # self.root.ids['create_video'].disabled = True
             self.root.ids['quit'].disabled = True
         else:  # False -> stop timelapse
+            self.toggle_preview(on=True)
             self.root.ids['timelapse_toggle'].text = 'Start Timelapse'
             self.root.ids['preview'].source = "1blackpixel.png"
-            self.root.ids['infos'].text = "Ready"
-            self.root.ids['toggle_preview'].disabled = False
-            self.root.ids['resolution_preview'].disabled = False
+            self.update_ui_time_info()
             self.root.ids['resolution_timelapse'].disabled = False
             self.root.ids['rotation'].disabled = False
             self.root.ids['hflip'].disabled = False
@@ -286,8 +272,7 @@ class PylapseApp(App):
             self.root.ids['show_images'].disabled = False
             self.root.ids['add_time'].disabled = False
             self.root.ids['consistent_images'].disabled = False
-            self.root.ids['create_video'].disabled = False
-            self.root.ids['delete_images'].disabled = False
+            # self.root.ids['create_video'].disabled = False
             self.root.ids['quit'].disabled = False
 
     def perform_capture(self):
@@ -318,6 +303,6 @@ class PylapseApp(App):
         os.system('rm images/*')
 
 
-# Run the app
+# Run the app -------
 app = PylapseApp()
 app.run()
