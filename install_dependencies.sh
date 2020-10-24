ff98676c852d9862eb1995c5644c1abbc57119d8
Olivier Boesch <olivier.boesch@ac-aix-marseille.fr>
2020-10-24 15:13:48 +0200

    added ffmpeg install with apt

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
+# ffmpeg install
+echo "ffmpeg install to create videos"
+sudo apt install ffmpeg
+
 # kivy install from master
 echo "Installing kivy from master branch"
 sudo apt install python3-pip build-essential git python3 python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
